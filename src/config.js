3732c6620ed4ed476d2ff0c528e556d49a25487d
Vital <vitalodev@gmail.com>
2019-10-14 13:52:03 +0300

    change config
    Don't show your secret data others :)

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-  port: 3000,
-  jwtSecret: 'this_is_jwt_secret_for_owwwls_work',
+  port: port,
+  jwtSecret: secret,
   gmail: {
-    user: 'owwwls.work@gmail.com',
-    password: 'so_secret_password_15.01.2019'
+    user: user,
+    password: password
   }
 }
